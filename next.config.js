964ffcda22a5fec63fa71bbc5579e115dccf69bc
Dhruv Barad <barad019@umn.edu>
2024-01-07 15:18:20 -0600

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx"],
+    pageExtensions: ["js", "jsx", "ts", "tsx"],
 }
 
 module.exports = nextConfig
